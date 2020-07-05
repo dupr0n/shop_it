712c740cd3dea8425252950599799e74651d0c7e
rkdupr0n <rkdupr0n@users.noreply.github.com>
2020-07-05 13:27:41 +0530

    Using AnimatedBuilder

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -160,8 +160,8 @@ class _AuthCardState extends State<AuthCard>
     _signInUpTransition = Tween<Size>(
       begin: Size(double.infinity, 260),
       end: Size(double.infinity, 320),
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInCubic));
-    _signInUpTransition.addListener(() => setState(() {}));
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
+    //? _signInUpTransition.addListener(() => setState(() {}));
     super.initState();
   }
 
@@ -173,92 +173,93 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        height: _signInUpTransition.value.height,
-        constraints:
-            BoxConstraints(minHeight: _signInUpTransition.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
+      child: AnimatedBuilder(
+        animation: _signInUpTransition,
+        builder: (ctx, child) => Container(
+          height: _signInUpTransition.value.height,
+          constraints:
+              BoxConstraints(minHeight: _signInUpTransition.value.height),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: Form(key: _formKey, child: child),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'E-Mail'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  return (value.isEmpty || !value.contains('@'))
+                      ? 'Invalid email!'
+                      : null;
+                },
+                onSaved: (value) {
+                  _authData['email'] = value;
+                },
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) =>
+                    FocusScope.of(context).requestFocus(_pass),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                controller: _passwordController,
+                validator: (value) {
+                  return (value.isEmpty || value.length < 5)
+                      ? 'Password is too short!'
+                      : null;
+                },
+                onSaved: (value) {
+                  _authData['password'] = value;
+                },
+                focusNode: _pass,
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) =>
+                    FocusScope.of(context).requestFocus(_repass),
+              ),
+              if (_authMode == AuthMode.Signup)
                 TextFormField(
-                  decoration: InputDecoration(labelText: 'E-Mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    return (value.isEmpty || !value.contains('@'))
-                        ? 'Invalid email!'
-                        : null;
-                  },
-                  onSaved: (value) {
-                    _authData['email'] = value;
-                  },
-                  textInputAction: TextInputAction.next,
-                  onFieldSubmitted: (_) =>
-                      FocusScope.of(context).requestFocus(_pass),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Password'),
+                  enabled: _authMode == AuthMode.Signup,
+                  decoration: InputDecoration(labelText: 'Confirm Password'),
                   obscureText: true,
-                  controller: _passwordController,
-                  validator: (value) {
-                    return (value.isEmpty || value.length < 5)
-                        ? 'Password is too short!'
-                        : null;
-                  },
-                  onSaved: (value) {
-                    _authData['password'] = value;
-                  },
-                  focusNode: _pass,
-                  textInputAction: TextInputAction.next,
-                  onFieldSubmitted: (_) =>
-                      FocusScope.of(context).requestFocus(_repass),
+                  validator: _authMode == AuthMode.Signup
+                      ? (value) {
+                          return (value != _passwordController.text)
+                              ? 'Passwords do not match!'
+                              : null;
+                        }
+                      : null,
+                  focusNode: _repass,
+                  textInputAction: TextInputAction.done,
                 ),
-                if (_authMode == AuthMode.Signup)
-                  TextFormField(
-                    enabled: _authMode == AuthMode.Signup,
-                    decoration: InputDecoration(labelText: 'Confirm Password'),
-                    obscureText: true,
-                    validator: _authMode == AuthMode.Signup
-                        ? (value) {
-                            return (value != _passwordController.text)
-                                ? 'Passwords do not match!'
-                                : null;
-                          }
-                        : null,
-                    focusNode: _repass,
-                    textInputAction: TextInputAction.done,
+              SizedBox(
+                height: 20,
+              ),
+              if (_isLoading)
+                CircularProgressIndicator()
+              else
+                RaisedButton(
+                  child:
+                      Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
+                  onPressed: _submit,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
                   ),
-                SizedBox(
-                  height: 20,
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+                  color: Theme.of(context).primaryColor,
+                  textColor: Theme.of(context).primaryTextTheme.button.color,
                 ),
-                if (_isLoading)
-                  CircularProgressIndicator()
-                else
-                  RaisedButton(
-                    child:
-                        Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
-                    onPressed: _submit,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
-                    color: Theme.of(context).primaryColor,
-                    textColor: Theme.of(context).primaryTextTheme.button.color,
-                  ),
-                FlatButton(
-                  child: Text(
-                      '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
-                  onPressed: _switchAuthMode,
-                  padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
-                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  textColor: Theme.of(context).primaryColor,
-                ),
-              ],
-            ),
+              FlatButton(
+                child: Text(
+                    '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
+                onPressed: _switchAuthMode,
+                padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                textColor: Theme.of(context).primaryColor,
+              ),
+            ],
           ),
         ),
       ),
@@ -267,7 +268,7 @@ class _AuthCardState extends State<AuthCard>
 
   @override
   void dispose() {
-    _controller.dispose();
+    //? _controller.dispose();
     super.dispose();
   }
 }
